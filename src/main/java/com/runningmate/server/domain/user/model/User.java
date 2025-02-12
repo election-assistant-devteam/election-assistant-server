1405becf6d8231aaeef80bdedccaefd1ffbdcce6
JangIkhwan <bluensky0213@gmail.com>
2025-02-13 02:07:51 +0900

    [Feat] #2 User 엔티티에 빌더패턴 적용

--- src/main/java/com/runningmate/server/domain/user/model/User.java
+++ src/main/java/com/runningmate/server/domain/user/model/User.java
@@ -2,9 +2,11 @@ package com.runningmate.server.domain.user.model;
 
 import com.runningmate.server.global.common.model.BaseEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
+@Builder
 @Entity
 @SQLDelete(sql = "UPDATE user SET status='N' where id = ?")
 @SQLRestriction("status = 'Y'")
