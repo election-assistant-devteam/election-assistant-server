27d9a2767111de873afcae6ca0b000345f54f97c
JangIkhwan <bluensky0213@gmail.com>
2025-02-13 02:06:44 +0900

    [Feat] #2 기존회원인지 검증하기 위해서 리포지토리에 메소드 추가

--- src/main/java/com/runningmate/server/domain/user/repository/UserRepository.java
+++ src/main/java/com/runningmate/server/domain/user/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.runningmate.server.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
