b860c55487c8986a6332e323722c123c3980a353
JangIkhwan <bluensky0213@gmail.com>
2025-02-13 01:34:58 +0900

    [Feat] #2 UserRepository 작성

---
+++ src/main/java/com/runningmate/server/domain/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.runningmate.server.domain.user.repository;
+
+import com.runningmate.server.domain.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
