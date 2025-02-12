eb7865fa90aa09210c572f8e4f480efcac66927b
JangIkhwan <bluensky0213@gmail.com>
2025-02-13 01:34:32 +0900

    [Feat] #2 User 엔티티 구현

---
+++ src/main/java/com/runningmate/server/domain/user/model/User.java
@@ -0,0 +1,27 @@
+package com.runningmate.server.domain.user.model;
+
+import com.runningmate.server.global.common.model.BaseEntity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@SQLDelete(sql = "UPDATE user SET status='N' where id = ?")
+@SQLRestriction("status = 'Y'")
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 20, nullable = false)
+    private String username;
+    @Column(length = 20, nullable = false)
+    private String password;
+    @Column(length = 20, nullable = false)
+    private String nickname;
+    @Column(length = 320, nullable = false)
+    private String email;
+    @Column(length = 100)
+    private String partyOfInterest;
+    @Column(length = 100)
+    private String politicianOfInterest;
+}
