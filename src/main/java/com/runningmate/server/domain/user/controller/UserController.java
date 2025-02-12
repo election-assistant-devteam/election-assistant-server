b1bf85d1c2d126f35912eb41351b5a467c71d705
JangIkhwan <bluensky0213@gmail.com>
2025-02-13 02:09:19 +0900

    [Feat] #2 컨트롤러에 생성자주입용 생성자 추가

--- src/main/java/com/runningmate/server/domain/user/controller/UserController.java
+++ src/main/java/com/runningmate/server/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.runningmate.server.domain.user.controller;
 import com.runningmate.server.domain.user.dto.CreateUserRequest;
 import com.runningmate.server.domain.user.service.UserService;
 import com.runningmate.server.global.common.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("users")
 public class UserController {
+    private final UserService userService;
     @PostMapping
     public BaseResponse<Void> createUser(@RequestBody CreateUserRequest request){
         log.info("[postUser] request = {}", request);
