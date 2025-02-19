2a3f4bf3984f598a10b460f10a28c0c76f05d3ae
JangIkhwan <bluensky0213@gmail.com>
2025-02-19 22:39:05 +0900

    [Feat] #2 DTO 검증 추가

--- src/main/java/com/runningmate/server/domain/user/controller/UserController.java
+++ src/main/java/com/runningmate/server/domain/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.runningmate.server.domain.user.controller;
 import com.runningmate.server.domain.user.dto.CreateUserRequest;
 import com.runningmate.server.domain.user.service.UserService;
 import com.runningmate.server.global.common.response.BaseResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
     @PostMapping
-    public BaseResponse<Void> createUser(@RequestBody CreateUserRequest request){
+    public BaseResponse<Void> createUser(@RequestBody @Valid CreateUserRequest request){
         log.info("[postUser] request = {}", request);
         userService.createUser(request.getUsername(), request.getPassword(), request.getNickname(), request.getEmail());
         return new BaseResponse<>(null);
