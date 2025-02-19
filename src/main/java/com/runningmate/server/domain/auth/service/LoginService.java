02b0401ad584b0ab56b9672205e763ddd22da174
JangIkhwan <bluensky0213@gmail.com>
2025-02-20 00:22:12 +0900

    [Feat] #2 로그인 실패 예외 구현

--- src/main/java/com/runningmate/server/domain/auth/service/LoginService.java
+++ src/main/java/com/runningmate/server/domain/auth/service/LoginService.java
@@ -2,6 +2,7 @@ package com.runningmate.server.domain.auth.service;
 
 import com.runningmate.server.domain.auth.dto.LoginRequest;
 import com.runningmate.server.domain.auth.dto.LoginResponse;
+import com.runningmate.server.domain.auth.exception.LoginFailedException;
 import com.runningmate.server.domain.user.model.User;
 import com.runningmate.server.domain.user.repository.UserRepository;
 import com.runningmate.server.global.common.exception.UserNotFoundException;
@@ -9,6 +10,9 @@ import com.runningmate.server.global.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import static com.runningmate.server.global.common.response.status.BaseExceptionResponseStatus.INCORRECT_PASSWORD;
+import static com.runningmate.server.global.common.response.status.BaseExceptionResponseStatus.INCORRECT_USERNAME;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,10 +25,10 @@ public class LoginService {
         String username = request.getUsername();
         String password = request.getPassword();
 
-        User user = userRepository.findByUsername(username).orElseThrow(()->new IllegalArgumentException("아이디가 일치하는 회원이 없습니다."));
+        User user = userRepository.findByUsername(username).orElseThrow(()->new LoginFailedException(INCORRECT_USERNAME));
 
         if(!user.hasSamePassword(password)){
-            throw new IllegalArgumentException("비밀번호가 일치하는 회원이 없습니다.");
+            throw new LoginFailedException(INCORRECT_PASSWORD);
         }
 
         String accessToken = jwtUtil.generateAccessToken(user.getId());
--- src/main/java/com/runningmate/server/global/common/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/com/runningmate/server/global/common/response/status/BaseExceptionResponseStatus.java
@@ -10,6 +10,14 @@ public enum BaseExceptionResponseStatus implements ResponseStatus{
 
     UNATHORIZED_USER(40100,  "권한이 없는 사용자의 요청입니다."),
 
+    // 엔티티 관련 에러
+    USER_NOT_FOUND(40400, "존재하지 않는 유저입니다."),
+
+    // auth 도메인 에러
+    INCORRECT_USERNAME(40400, "아이디가 일치하는 회원이 없습니다"),
+    INCORRECT_PASSWORD(40400, "비밀번호가 일치하지 않습니다"),
+    LOGIN_FAILED(40400, "아이디나 비밀번호가 일치하는 회원이 없습니다."),
+
     // 유저 관련 에러
     SAME_USERNAME_EXISTS(40000, "동일한 아이디를 가진 사용자가 존재합니다."),
     SAME_USER_EMAIL_EXISTS(40000, "동일한 이메일을 가진 사용자가 존재합니다."),
