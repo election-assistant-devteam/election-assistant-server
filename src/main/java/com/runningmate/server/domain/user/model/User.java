67fab54b8e2f33009a0b8ba7fc6c8beb59d8b0d0
JangIkhwan <bluensky0213@gmail.com>
2025-02-20 00:08:40 +0900

    [Feat] #2 User엔티티에 비밀번호 일치 여부 확인 메소드 추가

--- src/main/java/com/runningmate/server/domain/user/model/User.java
+++ src/main/java/com/runningmate/server/domain/user/model/User.java
@@ -2,10 +2,16 @@ package com.runningmate.server.domain.user.model;
 
 import com.runningmate.server.global.common.model.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Table(name = "users")
 @Entity
@@ -27,4 +33,8 @@ public class User extends BaseEntity {
     private String partyOfInterest;
     @Column(length = 100)
     private String politicianOfInterest;
+
+    public boolean hasSamePassword(String password) {
+        return this.password.equals(password);
+    }
 }
