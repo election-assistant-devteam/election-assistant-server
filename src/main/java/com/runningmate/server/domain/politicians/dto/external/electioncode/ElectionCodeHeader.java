a98288103f21ec5fb400d4ad384b86b0cbf50864
강병현 <kim1228com@naver.com>
2025-02-23 11:40:20 +0900

    [feat] 외부api 선거 코드 dto에서 Header부분도 추가

--- src/main/java/com/runningmate/server/domain/politicians/dto/external/electioncode/ElectionCodeHeader.java
+++ src/main/java/com/runningmate/server/domain/politicians/dto/external/electioncode/ElectionCodeHeader.java
@@ -1,4 +1,15 @@
 package com.runningmate.server.domain.politicians.dto.external.electioncode;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ElectionCodeHeader {
+    @JsonProperty("resultCode")
+    private String resultCode;
+
+    @JsonProperty("resultMsg")
+    private String resultMsg;
 }
