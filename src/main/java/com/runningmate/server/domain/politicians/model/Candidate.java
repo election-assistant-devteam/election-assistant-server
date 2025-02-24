e5e24ef3682e14d72c436f415e4a12bda52f4896
강병현 <kim1228com@naver.com>
2025-02-24 20:30:23 +0900

    [feat] Candidate 엔티티 추가, [refactor] 클래스 이름 변경

--- src/main/java/com/runningmate/server/domain/politicians/model/Candidate.java
+++ src/main/java/com/runningmate/server/domain/politicians/model/Candidate.java
@@ -1,23 +1,16 @@
 package com.runningmate.server.domain.politicians.model;
 
-import com.runningmate.server.global.common.model.BaseEntity;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
-
-import java.util.Date;
 
 @Entity
 @SQLDelete(sql = "UPDATE user SET status='N' where id = ?")
 @SQLRestriction("status = 'Y'")
-public class Elections extends BaseEntity {
+public class Candidate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(length = 100, nullable = false)
-    private String name;
-    @Column(nullable = false)
-    private Date date;
+    @Column(length = 50, nullable = false)
+    private String affiliatedPartyAtTheTime;
 }
---
+++ src/main/java/com/runningmate/server/domain/politicians/model/Election.java
@@ -0,0 +1,21 @@
+package com.runningmate.server.domain.politicians.model;
+
+import com.runningmate.server.global.common.model.BaseEntity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.util.Date;
+
+@Entity
+@SQLDelete(sql = "UPDATE user SET status='N' where id = ?")
+@SQLRestriction("status = 'Y'")
+public class Election extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 100, nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Date date;
+}
