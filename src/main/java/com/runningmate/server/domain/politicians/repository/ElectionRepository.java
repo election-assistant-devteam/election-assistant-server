0ccb0fb3e69afc86b5d4917d3695f6659cf122c2
강병현 <kim1228com@naver.com>
2025-02-24 21:37:12 +0900

    [chore] 잘못된 철자 추가 고침

--- src/main/java/com/runningmate/server/domain/politicians/repository/ElectionRepository.java
+++ src/main/java/com/runningmate/server/domain/politicians/repository/ElectionRepository.java
@@ -3,5 +3,5 @@ package com.runningmate.server.domain.politicians.repository;
 import com.runningmate.server.domain.politicians.model.Election;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ElectionRepository extends JpaRepository<Election, Longg> {
+public interface ElectionRepository extends JpaRepository<Election, Long> {
 }
