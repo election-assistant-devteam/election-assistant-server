630b50838e05ef57eeda167c48ea552a75b365f5
강병현 <kim1228com@naver.com>
2025-02-25 04:29:51 +0900

    [feat] findByDateAndType 추가

--- src/main/java/com/runningmate/server/domain/politicians/repository/ElectionRepository.java
+++ src/main/java/com/runningmate/server/domain/politicians/repository/ElectionRepository.java
@@ -3,5 +3,9 @@ package com.runningmate.server.domain.politicians.repository;
 import com.runningmate.server.domain.politicians.model.Election;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.Optional;
+
 public interface ElectionRepository extends JpaRepository<Election, Long> {
+    Optional<Election> findByDateAndType(Date date, String type);
 }
