fa23f9cb3629bd4ae6290acc5830e7075be29184
강병현 <kim1228com@naver.com>
2025-02-24 21:33:46 +0900

    [feat] ElectionRepository 추가

---
+++ src/main/java/com/runningmate/server/domain/politicians/repository/ElectionRepository.java
@@ -0,0 +1,7 @@
+package com.runningmate.server.domain.politicians.repository;
+
+import com.runningmate.server.domain.politicians.model.Election;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ElectionRepository extends JpaRepository<Election, Longg> {
+}
