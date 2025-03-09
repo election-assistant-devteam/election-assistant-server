f4303601d6c65f43bf3dcca8592c5c3f2adf6cc8
JangIkhwan <bluensky0213@gmail.com>
2025-03-09 17:10:03 +0900

    [Feat] #19 LoginUserIdArgumentResolver를 WebConfig에 등록

--- src/main/java/com/runningmate/server/global/jwt/LoginUserIdArgumentResolver.java
+++ src/main/java/com/runningmate/server/global/jwt/LoginUserIdArgumentResolver.java
@@ -4,12 +4,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 @Slf4j
+@Component
 @RequiredArgsConstructor
 public class LoginUserIdArgumentResolver implements HandlerMethodArgumentResolver {
     private final JwtUtil jwtUtil;
