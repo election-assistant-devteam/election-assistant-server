2cf926487b6f6d242b2f8fba3a768abfe6b969d5
강병현 <kim1228com@naver.com>
2025-03-09 17:19:09 +0900

    [feat] #21 선거로 후보자 조회 기능 추가

--- src/main/java/com/runningmate/server/domain/politicians/repository/CandidateRepository.java
+++ src/main/java/com/runningmate/server/domain/politicians/repository/CandidateRepository.java
@@ -5,8 +5,11 @@ import com.runningmate.server.domain.politicians.model.Election;
 import com.runningmate.server.domain.politicians.model.Politician;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
     Optional<Candidate> findByPoliticianAndElection(Politician politician, Election election);
+
+    List<Candidate> findByElection(Election election);
 }
