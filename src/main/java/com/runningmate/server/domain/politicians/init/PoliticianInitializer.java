cfabef42d7ee87a58233d1ca499fb44dbabc7618
강병현 <kim1228com@naver.com>
2025-05-26 01:28:48 +0900

    [Refactor] #48 애플리케이션 실행 시점에 선거 데이터 가져오도록 변경
    - 기존에 api를 통해서 선거 데이터를 가져오는 구조에서 애플리케이션 실행
      시점에 선거 데이터를 가져오도록 변경

---
+++ src/main/java/com/runningmate/server/domain/politicians/init/PoliticianInitializer.java
@@ -0,0 +1,28 @@
+package com.runningmate.server.domain.politicians.init;
+
+import com.runningmate.server.domain.politicians.service.NationalAssemblyService;
+import com.runningmate.server.domain.politicians.service.PoliticianInfoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PoliticianInitializer implements ApplicationRunner {
+
+    private final PoliticianInfoService politicianInfoService;
+    private final NationalAssemblyService nationalAssemblyService;
+
+    @Override
+    public void run(ApplicationArguments args){
+        politicianInfoService.savePoliticianInfos();
+        log.info("기본 저장 완료");
+
+        nationalAssemblyService.saveMemberImg();
+        log.info("정치인 사진 저장 완료");
+    }
+
+}
