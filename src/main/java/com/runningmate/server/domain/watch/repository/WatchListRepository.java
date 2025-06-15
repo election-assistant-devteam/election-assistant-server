3b2d64d41990be0bbc4bce2e90cc39e417fff706
강병현 <kim1228com@naver.com>
2025-06-16 04:12:10 +0900

    [Feat] #56 특정 정치인을 특정 유저가 지켜보는지 여부 확인 메서드 추가

---
+++ src/main/java/com/runningmate/server/domain/watch/repository/WatchListRepository.java
@@ -0,0 +1,16 @@
+package com.runningmate.server.domain.watch.repository;
+
+import com.runningmate.server.domain.watch.model.UserWatchList;
+import com.runningmate.server.domain.watch.model.UserWatchListId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WatchListRepository extends JpaRepository<UserWatchList, UserWatchListId> {
+
+    // 특정 정치인을 특정 유저가 지켜보는지 여부
+    boolean existsById(UserWatchListId id);
+
+}
