83affff804bfee2ac31bde97032814e1744e6938
강병현 <kim1228com@naver.com>
2025-06-16 04:17:46 +0900

    [Feat] #56 지켜보기 정치인 추가 응답 DTO 추가

---
+++ src/main/java/com/runningmate/server/domain/watch/dto/AddWatchListResponse.java
@@ -0,0 +1,10 @@
+package com.runningmate.server.domain.watch.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AddWatchListResponse {
+    private Long politicianId;
+}
