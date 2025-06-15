014704c7ded9b71ad9b2e53b9b7e078141a12a2f
강병현 <kim1228com@naver.com>
2025-06-16 04:37:04 +0900

    [chore] import 애노테이션

--- src/main/java/com/runningmate/server/domain/politicians/controller/internal/CandidateController.java
+++ src/main/java/com/runningmate/server/domain/politicians/controller/internal/CandidateController.java
@@ -5,10 +5,7 @@ import com.runningmate.server.domain.politicians.service.CandidateService;
 import com.runningmate.server.global.common.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
