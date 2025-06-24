ca0c799583491a231b0eb65819974718f9973640
Chris Ahn <chrisahn1007@gmail.com>
2025-06-25 00:56:21 +0900

    fix : 뉴스 서비스 코드 수정

--- src/main/java/com/runningmate/server/domain/news/service/NewsService.java
+++ src/main/java/com/runningmate/server/domain/news/service/NewsService.java
@@ -22,7 +22,7 @@ public class NewsService {
 
         ChromeOptions options = new ChromeOptions();
         options.setBinary("/usr/bin/chromium-browser");
-        options.addArguments("--headless=new");
+        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
