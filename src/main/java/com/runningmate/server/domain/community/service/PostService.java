6d7571ba75cfd985e31e437b45e6481fb031dd9a
JangIkhwan <bluensky0213@gmail.com>
2025-06-26 21:11:08 +0900

    [Feat] #66 서비스에 일기 생성 메소드 추가

--- src/main/java/com/runningmate/server/domain/community/service/PostService.java
+++ src/main/java/com/runningmate/server/domain/community/service/PostService.java
@@ -1,12 +1,61 @@
 package com.runningmate.server.domain.community.service;
 
+import com.runningmate.server.domain.community.dto.CreatePostRequest;
+import com.runningmate.server.domain.community.infrastructure.S3Uploader;
+import com.runningmate.server.domain.community.model.Image;
+import com.runningmate.server.domain.community.model.Post;
+import com.runningmate.server.domain.community.repository.PostRepository;
+import com.runningmate.server.domain.user.model.User;
+import com.runningmate.server.domain.user.repository.UserRepository;
+import com.runningmate.server.global.common.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.runningmate.server.global.common.response.status.BaseExceptionResponseStatus.USER_NOT_FOUND;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class PostService {
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final S3Uploader s3Uploader;
+    public long create(long userId, List<MultipartFile> files, CreatePostRequest request) {
+        log.info("[create]");
 
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
+
+        // 이미지 리스트를 s3에 저장
+        List<Image> images = new ArrayList<>();
+        for (int i = 0; i < files.size(); i++) {
+            MultipartFile file = files.get(i);
+            String imageUrl = null;
+            try {
+                imageUrl = s3Uploader.upload(file);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            images.add(Image.builder()
+                    .imageUrl(imageUrl)
+                    .imageOrder(i + 1)
+                    .build());
+        }
+
+        // 게시물을 저장
+        Post post = Post.builder()
+                .title(request.title())
+                .content(request.content())
+                .images(images)
+                .writer(user)
+                .build();
+
+        Post save = postRepository.save(post);
+        return save.getId();
+    }
 }
