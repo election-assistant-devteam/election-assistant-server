3f4b95f178fa0d933fcbd89cb122e0a5fd8b1819
JangIkhwan <bluensky0213@gmail.com>
2025-06-27 08:23:11 +0900

    [Fix] #66 연관관계 설정 수정 및 리팩토링

--- src/main/java/com/runningmate/server/domain/community/service/PostService.java
+++ src/main/java/com/runningmate/server/domain/community/service/PostService.java
@@ -8,6 +8,7 @@ import com.runningmate.server.domain.community.repository.PostRepository;
 import com.runningmate.server.domain.user.model.User;
 import com.runningmate.server.domain.user.repository.UserRepository;
 import com.runningmate.server.global.common.exception.UserNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,7 @@ import static com.runningmate.server.global.common.response.status.BaseException
 
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class PostService {
     private final UserRepository userRepository;
@@ -32,6 +34,23 @@ public class PostService {
         User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND));
 
         // 이미지 리스트를 s3에 저장
+        List<Image> images = uploadImagesToS3(files);
+
+        // 게시물을 저장
+        Post post = Post.builder()
+                .title(request.title())
+                .content(request.content())
+                .writer(user)
+                .build();
+
+        images.stream().forEach(image -> image.setPost(post));
+
+        Post save = postRepository.save(post);
+
+        return save.getId();
+    }
+
+    private List<Image> uploadImagesToS3(List<MultipartFile> files) {
         List<Image> images = new ArrayList<>();
         for (int i = 0; i < files.size(); i++) {
             MultipartFile file = files.get(i);
@@ -46,16 +65,6 @@ public class PostService {
                     .imageOrder(i + 1)
                     .build());
         }
-
-        // 게시물을 저장
-        Post post = Post.builder()
-                .title(request.title())
-                .content(request.content())
-                .images(images)
-                .writer(user)
-                .build();
-
-        Post save = postRepository.save(post);
-        return save.getId();
+        return images;
     }
 }
