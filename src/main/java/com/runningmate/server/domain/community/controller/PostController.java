4da510cc5f3a4a7e498fe548219e081685754832
JangIkhwan <bluensky0213@gmail.com>
2025-06-28 01:30:53 +0900

    [Feat] #72 컨트롤러에 게시글 목록 조회 메서드 추가

--- src/main/java/com/runningmate/server/domain/community/controller/PostController.java
+++ src/main/java/com/runningmate/server/domain/community/controller/PostController.java
@@ -2,6 +2,7 @@ package com.runningmate.server.domain.community.controller;
 
 import com.runningmate.server.domain.community.dto.CreateCommentOnPostRequest;
 import com.runningmate.server.domain.community.dto.CreatePostRequest;
+import com.runningmate.server.domain.community.dto.GetPostsResponse;
 import com.runningmate.server.domain.community.service.PostService;
 import com.runningmate.server.global.common.response.BaseResponse;
 import com.runningmate.server.global.jwt.LoginUserId;
@@ -31,6 +32,12 @@ public class PostController {
         log.info("[createCommentOnPost] userId={}", userId);
         long commmentId = postService.createComment(userId, postId, request);
         return new BaseResponse<>(Map.of("commentId", commmentId));
+    }
+
+    @GetMapping
+    public BaseResponse<GetPostsResponse> GetPosts(@LoginUserId Long userId, @RequestParam(required = false) Long lastId, @RequestParam(required = false) String keyword){
+        log.info("[findPosts] userId = {} lastId = {} keyword = {}", userId, lastId, keyword);
+        return new BaseResponse<>(postService.findPageByCursor(lastId, keyword, 10));
     }
 }
 
