90233097ef521c523b0a07d257d38ba1043a2068
JangIkhwan <bluensky0213@gmail.com>
2025-06-28 01:36:17 +0900

    [Feat] #72 레포지토리에 게시글 커서페이징용 메서드 추가

--- src/main/java/com/runningmate/server/domain/community/repository/PostRepository.java
+++ src/main/java/com/runningmate/server/domain/community/repository/PostRepository.java
@@ -1,10 +1,21 @@
 package com.runningmate.server.domain.community.repository;
 
 import com.runningmate.server.domain.community.model.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    @Query("""
+            select p from Post p
+            where (:keyword is null or :keyword like '' or p.content like %:keyword% or p.title like %:keyword% )
+            and (:lastId is null or p.id < :lastId)
+            order by p.id desc
+    """)
+    List<Post> findWithKeywordAndCursor(@Param("keyword")String keyword, @Param("lastId") Long lastId, Pageable pageable);
 }
