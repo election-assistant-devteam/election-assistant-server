0e5bd44c92b55e83f0b2e360b60d5f164465c654
Chris Ahn <77713368+kyuris01@users.noreply.github.com>
2025-06-28 22:54:53 +0900

    Update BaseExceptionResponseStatus.java

--- src/main/java/com/runningmate/server/global/common/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/com/runningmate/server/global/common/response/status/BaseExceptionResponseStatus.java
@@ -37,7 +37,7 @@ public enum BaseExceptionResponseStatus implements ResponseStatus{
     FIND_ELECTION_FAILED(40400, "존재하지 않는 선거입니다."),
 
     // 정치인 관련 에러
-    FIND_POLITICIAN_FAILED(40400, "존재하지 않는 정치인입니다.");
+    POLITICIAN_NOT_FOUND(40400, "존재하지 않는 정치인입니다.");
 
     private final boolean success = false;
     private final int code;
