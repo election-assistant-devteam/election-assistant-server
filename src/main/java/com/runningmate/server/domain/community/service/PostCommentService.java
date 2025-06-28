ce30082607ecc0bef9520f58aee5a26c7ec23342
JangIkhwan <bluensky0213@gmail.com>
2025-06-28 17:03:00 +0900

    [Feat] #72 PostCommentService 추가

---
+++ src/main/java/com/runningmate/server/domain/community/service/PostCommentService.java
@@ -0,0 +1,9 @@
+package com.runningmate.server.domain.community.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PostCommentService {
+}
