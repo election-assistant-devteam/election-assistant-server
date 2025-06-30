e7113d87f1e23d8b16d3e0bce2e13244ff6e3a56
JangIkhwan <bluensky0213@gmail.com>
2025-06-30 15:17:48 +0900

    [Remove] #79 ElectionNotFoundException 삭제

--- src/main/java/com/runningmate/server/domain/politicians/exception_handler/PoliticianControllerAdvice.java
+++ src/main/java/com/runningmate/server/domain/politicians/exception_handler/PoliticianControllerAdvice.java
@@ -1,8 +1,6 @@
 package com.runningmate.server.domain.politicians.exception_handler;
 
-import com.runningmate.server.domain.politicians.exception.ElectionNotFoundException;
 import com.runningmate.server.domain.politicians.exception.ParsingFailedException;
-import com.runningmate.server.domain.user.exception.SameUserExistsException;
 import com.runningmate.server.global.common.response.BaseErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,14 +13,6 @@ import static com.runningmate.server.global.common.response.status.BaseException
 @Slf4j
 @RestControllerAdvice
 public class PoliticianControllerAdvice {
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(ElectionNotFoundException.class)
-    public BaseErrorResponse handle_ElectionNotFoundException(ElectionNotFoundException e){
-        log.info("[handle_ElectionNotFoundException]");
-        return new BaseErrorResponse(FIND_ELECTION_FAILED);
-    }
-
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(ParsingFailedException.class)
     public BaseErrorResponse handle_ParsingFailedException(ParsingFailedException e){
