5b848b27a57383ab02b35fdcbf3f6f5c2225b2c1
JangIkhwan <bluensky0213@gmail.com>
2025-07-01 15:37:35 +0900

    [Feat] #83 Comment에 댓글 공감 메서드 추가

--- src/main/java/com/runningmate/server/domain/community/model/Comment.java
+++ src/main/java/com/runningmate/server/domain/community/model/Comment.java
@@ -1,6 +1,7 @@
 package com.runningmate.server.domain.community.model;
 
 import com.runningmate.server.domain.user.model.User;
+import com.runningmate.server.global.common.exception.BadRequestException;
 import com.runningmate.server.global.common.model.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -12,6 +13,8 @@ import org.hibernate.annotations.SQLRestriction;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.runningmate.server.global.common.response.status.BaseExceptionResponseStatus.WRITER_NOT_ALLOWED_TO_ADD_LIKE;
 
 @Getter
 @Builder
@@ -61,5 +64,18 @@ public class Comment extends BaseEntity {
 
     public boolean isParent(){
         return this.parent == null;
+    }
+
+    public CommentLike addLike(User user) {
+        if(this.writer == user){
+            throw new BadRequestException(WRITER_NOT_ALLOWED_TO_ADD_LIKE);
+        }
+
+        this.likeCount++;
+
+        return CommentLike.builder()
+                .comment(this)
+                .user(user)
+                .build();
     }
 }
