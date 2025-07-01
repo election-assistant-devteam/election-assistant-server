b24851c679d1c4b960ff8eb82d7126c3679763af
JangIkhwan <bluensky0213@gmail.com>
2025-07-01 21:24:38 +0900

    [Fix] #83 오타 수정

--- src/main/java/com/runningmate/server/global/common/response/status/BaseExceptionResponseStatus.java
+++ src/main/java/com/runningmate/server/global/common/response/status/BaseExceptionResponseStatus.java
@@ -44,7 +44,7 @@ public enum BaseExceptionResponseStatus implements ResponseStatus{
     COMMENT_NOT_FOUND(40400, "존재하지 않는 댓글입니다"),
     LIKE_NOT_ALLOWED_TO_WRITER(40000, "작성자는 자신의 글에 공감을 추가할 수 없습니다"),
     ALREADY_LIKED_POST(40000, "이미 공감을 누른 게시글입니다."),
-    ALREADY_LIKED_COMMENT(40000, "이미 공감을 누른 댓글입니다.");;
+    ALREADY_LIKED_COMMENT(40000, "이미 공감을 누른 댓글입니다.");
 
 
     private final boolean success = false;
