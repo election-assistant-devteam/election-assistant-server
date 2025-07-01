662d0aef6b4cf1a132f2d6c823e172a5a9573339
JangIkhwan <bluensky0213@gmail.com>
2025-07-01 16:23:31 +0900

    [Feat] #83 GetPostCommentsReponse에서 정적 팩토리 메서드 삭제

--- src/main/java/com/runningmate/server/domain/community/dto/GetPostCommentsReponse.java
+++ src/main/java/com/runningmate/server/domain/community/dto/GetPostCommentsReponse.java
@@ -8,9 +8,4 @@ import java.util.stream.Collectors;
 public record GetPostCommentsReponse(
     List<CommentResponse> comments
 ) {
-    public static GetPostCommentsReponse createFromEntityList(List<Comment> comments){
-        return new GetPostCommentsReponse(comments.stream()
-                .map(CommentResponse::entityToDto)
-                .collect(Collectors.toList()));
-    }
 }
