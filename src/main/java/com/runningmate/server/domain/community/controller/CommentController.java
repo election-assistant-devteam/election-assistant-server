ecd75f1b4341aa59396e97bb0c916f5e0fadcc27
JangIkhwan <bluensky0213@gmail.com>
2025-07-01 15:04:56 +0900

    [Feat] #83 CommentController 추가

---
+++ src/main/java/com/runningmate/server/domain/community/controller/CommentController.java
@@ -0,0 +1,11 @@
+package com.runningmate.server.domain.community.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("comments")
+@RestController
+public class CommentController {
+}
