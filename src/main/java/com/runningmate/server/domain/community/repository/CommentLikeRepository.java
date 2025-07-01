6fa39b12a541b22902640d9afc4013ba86d15a34
JangIkhwan <bluensky0213@gmail.com>
2025-07-01 15:23:38 +0900

    [Feat] #83 CommentLikeRepository 추가

---
+++ src/main/java/com/runningmate/server/domain/community/repository/CommentLikeRepository.java
@@ -0,0 +1,9 @@
+package com.runningmate.server.domain.community.repository;
+
+import com.runningmate.server.domain.community.model.CommentLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+}
