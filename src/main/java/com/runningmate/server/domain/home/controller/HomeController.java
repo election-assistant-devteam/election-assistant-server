a5ba0ed06b67c35d66ebabce441f985745376148
JangIkhwan <bluensky0213@gmail.com>
2025-07-04 17:36:32 +0900

    [Feat] #96 컨트롤러에 홈화면 조회 메서드 구현

--- src/main/java/com/runningmate/server/domain/home/controller/HomeController.java
+++ src/main/java/com/runningmate/server/domain/home/controller/HomeController.java
@@ -1,9 +1,23 @@
 package com.runningmate.server.domain.home.controller;
 
+import com.runningmate.server.domain.home.dto.GetHomeResponse;
+import com.runningmate.server.domain.home.service.HomeService;
+import com.runningmate.server.global.common.response.BaseResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RequiredArgsConstructor
+@RequestMapping("home")
 @RestController
 public class HomeController {
+    private final HomeService homeService;
+    @GetMapping
+    public BaseResponse<GetHomeResponse> getHome(){
+        log.info("[getHome]");
+        return new BaseResponse(homeService.getData());
+    }
 }
