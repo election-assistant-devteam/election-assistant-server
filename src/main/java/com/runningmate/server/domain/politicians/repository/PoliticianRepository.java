f4ef12f8bcff1bc536b73d304c9d25db09cc01b6
JangIkhwan <bluensky0213@gmail.com>
2025-07-04 21:19:03 +0900

    [Feat] #96 레포지토리에 정치인을 랜덤하게 조회하는 메소드를 추가

--- src/main/java/com/runningmate/server/domain/politicians/repository/PoliticianRepository.java
+++ src/main/java/com/runningmate/server/domain/politicians/repository/PoliticianRepository.java
@@ -26,4 +26,12 @@ public interface PoliticianRepository extends JpaRepository<Politician, Long> {
 
     @Query("SELECT DISTINCT p.party FROM Politician p")
     List<String> findAllParties();
+
+    @Query(value = """
+        SELECT * FROM politician
+        WHERE imageUrl IS NOT NULL
+        ORDER BY RAND()
+        LIMIT :size
+    """, nativeQuery = true)
+    List<Politician> findRandomPoliticians(@Param("size") int size);
 }
