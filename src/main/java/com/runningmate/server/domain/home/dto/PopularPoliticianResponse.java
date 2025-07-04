b074d65c13e9d5706945ee77406ef01f1f56c3f8
JangIkhwan <bluensky0213@gmail.com>
2025-07-04 21:21:29 +0900

    [Feat] #96 응답 DTO에 팩토리 메소드 추가

--- src/main/java/com/runningmate/server/domain/home/dto/PopularPoliticianResponse.java
+++ src/main/java/com/runningmate/server/domain/home/dto/PopularPoliticianResponse.java
@@ -1,8 +1,13 @@
 package com.runningmate.server.domain.home.dto;
 
+import com.runningmate.server.domain.politicians.model.Politician;
+
 public record PopularPoliticianResponse (
         Long politicianId,
         String name,
         String imageUrl
 ){
+    public static PopularPoliticianResponse entityToDto(Politician politician) {
+        return new PopularPoliticianResponse(politician.getId(), politician.getName(), politician.getImageUrl());
+    }
 }
