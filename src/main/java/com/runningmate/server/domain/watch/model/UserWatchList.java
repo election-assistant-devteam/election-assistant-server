af87b671d5e4931152a160ae2e272b6e23c01ef1
JangIkhwan <bluensky0213@gmail.com>
2025-07-29 19:42:43 +0900

    [Fix] #116 UserWatchList의 기본키를 Long타입으로 변경하고 엔티티가 BaseEntity를 상속받도록 수정

--- src/main/java/com/runningmate/server/domain/watch/model/UserWatchList.java
+++ src/main/java/com/runningmate/server/domain/watch/model/UserWatchList.java
@@ -2,6 +2,7 @@ package com.runningmate.server.domain.watch.model;
 
 import com.runningmate.server.domain.politicians.model.Politician;
 import com.runningmate.server.domain.user.model.User;
+import com.runningmate.server.global.common.model.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,23 +11,21 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class UserWatchList {
-    @EmbeddedId
-    private UserWatchListId id;
+public class UserWatchList extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("userId")
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId("politicianId")
     @JoinColumn(name = "politician_id", nullable = false)
     private Politician politician;
 
     @Builder
     public UserWatchList(User user, Politician politician) {
-        this.id = new UserWatchListId(user.getId(), politician.getId());
         this.user = user;
         this.politician = politician;
     }
