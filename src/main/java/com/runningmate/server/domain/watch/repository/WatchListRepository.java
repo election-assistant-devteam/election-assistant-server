df6b6b408edcdc54c179cd836abe8dc2b7a6ea5e
JangIkhwan <bluensky0213@gmail.com>
2025-07-29 20:07:38 +0900

    [Feat] #116 리포지토리에 나의 지켜보기 중인 정치인 조회 메서드 구현

--- src/main/java/com/runningmate/server/domain/watch/repository/WatchListRepository.java
+++ src/main/java/com/runningmate/server/domain/watch/repository/WatchListRepository.java
@@ -1,9 +1,9 @@
 package com.runningmate.server.domain.watch.repository;
 
 import com.runningmate.server.domain.politicians.model.Politician;
+import com.runningmate.server.domain.user.dto.MyWatchingPolitician;
 import com.runningmate.server.domain.watch.dto.PoliticianWithWatchCount;
 import com.runningmate.server.domain.watch.model.UserWatchList;
-import com.runningmate.server.domain.watch.model.UserWatchListId;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,10 +13,10 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface WatchListRepository extends JpaRepository<UserWatchList, UserWatchListId> {
+public interface WatchListRepository extends JpaRepository<UserWatchList, Long> {
 
     // 특정 정치인을 특정 유저가 지켜보는지 여부
-    boolean existsById(UserWatchListId id);
+    boolean existsByUserIdAndPoliticianId(Long userId, Long politicianID);
     List<UserWatchList> findByUser_Id(Long userId);
 
     @Query("""
@@ -35,4 +35,14 @@ public interface WatchListRepository extends JpaRepository<UserWatchList, UserWa
         order by count(w) desc
     """)
     List<PoliticianWithWatchCount> findTopPoliticiansByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query("""
+        select new com.runningmate.server.domain.user.dto.MyWatchingPolitician(uwl.id, p.id, p.party, p.name)
+        from UserWatchList uwl join uwl.politician p 
+        where uwl.user.id = :userId and (uwl.id < :lastId or :lastId is null)
+        order by uwl.id desc
+    """)
+    List<MyWatchingPolitician> findWatchingPoliticiansByCursor(Long userId, Long lastId, Pageable pageable);
+
+    void deleteByUserIdAndPoliticianId(Long userId, Long politicianId);
 }
